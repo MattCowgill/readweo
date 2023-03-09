27978a590d5c6d76224c246d7673eb4fdf94a916
“Matt <mattcowgill@gmail.com>
2023-03-09 15:33:13 +1100

    tweak test

--- tests/testthat/test-read_weo.R
+++ tests/testthat/test-read_weo.R
@@ -1,6 +1,5 @@
-test_that("read_weo() words", {
-  check_df <- function(month) {
-    df <- read_weo(month)
+test_that("read_weo() works", {
+  check_df <- function(df) {
     expect_s3_class(df, "tbl_df")
     expect_gt(ncol(df), 12)
     expect_gt(nrow(df), 100000)
@@ -8,8 +7,12 @@ test_that("read_weo() words", {
 
   for (month in c("Oct 2017", "april 2019", "october 2022")) {
     print(paste("Testing", month))
-    check_df(month)
+    df <- read_weo(month)
+    check_df(df)
   }
+
+  # Test default with no month specified
+  check_df(read_weo())
 
   expect_error(read_weo("Sep 2022"))
 })
